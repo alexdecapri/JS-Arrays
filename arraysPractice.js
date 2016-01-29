945cf44169c55c3176454b829fd25742a2c4ec08
alexdecapri <alexdecapri@gmail.com>
2016-01-28 21:07:40 -0700

    almost complete

--- arraysPractice.js
+++ arraysPractice.js
@@ -70,13 +70,14 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 //only argument and removes all values that aren't even from the 
 //given array.
 
-var newArr = [];
 function evenFinder(nums) {
+  var newArr = [];
   for (var i = 0; i < nums.length; i++) {
     if (nums[i] % 2 === 0) {
       newArr.push(nums[i]);
     };
   }
+  return newArr;
 }
 
 evenFinder(nums);
@@ -263,22 +264,34 @@ function longer(arr1, arr2) {
   if (arr1.length > arr2.length) {
     return arr1;
   }
-  else if (arr1.length < arr2.length) {
+  else {
     return arr2;
-  }
-  else if (arr1.length === arr2.length) {
-    return ("They are equal in length!");
   }
 }
 
 
-/*As a continuation of the previous problem, write another function called 'both'.
-  Your 'both' function will be given two arguments, arr1 and arr2 (from the previous example)
-  'both' should return a new array full of numbers that are found in both arr1 and arr2.
+/*
+As a continuation of the previous problem, write another function 
+called 'both'.Your 'both' function will be given two arguments, arr1 
+and arr2 (from the previous example)
+'both' should return a new array full of numbers that are found 
+in both arr1 and arr2.
 */
 
-  //Code Here
-  
+//join without repeating them
+
+function both(arr1, arr2) {
+  var newArr = [];
+  for (var i = 0; i < arr1.length; i++) {
+    for (var j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        newArr.push(arr1[i]);
+      }
+    }
+  }
+  return newArr;
+}
+
   
   
 
@@ -313,16 +326,24 @@ var colt = {
     spiritAnimal: 'Young Male Horse'
 };
 
-/*Above you're given an empty array with four objects. Fill the devMountainEmployees
-array with those four objects. After that console.log the length of the Array and make
-sure that it's equal to 4. */
+/*Above you're given an empty array with four objects. Fill the 
+devMountainEmployees array with those four objects. After that 
+console.log the length of the Array and make sure that it's equal to 4. 
+*/
 
-  //Code Here
+devMountainEmployees = [tyler, cahlan, ryan, colt];
+console.log(devMountainEmployees.length);
 
-/*Now let's say Cahlan has a mental breakdown and has to take a leave of absence to 'find himself'.
-Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
+/*Now let's say Cahlan has a mental breakdown and has to take a 
+leave of absence to 'find himself'.
+Loop through your devMountainEmployees until you find cahlan, 
+then remove him from the array.*/
 
-  //Code Here
+for (i = 0; i < devMountainEmployees.length; i++) {
+  if (devMountainEmployees[i] === cahlan) {
+    devMountainEmployees.splice([i], 1);
+  }
+}
 
 
 
@@ -332,12 +353,16 @@ Loop through your devMountainEmployees until you find cahlan, then remove him fr
 
 
 
-/*Now we're going to combine what we've learned today (objects) with what we learned
-yesterday (arrays). Yeah, take a deep breathe. You're ready for this, promise.
+/*
+Now we're going to combine what we've learned today (objects) with 
+what we learned yesterday (arrays). Yeah, take a deep breathe. 
+You're ready for this, promise.
 Let's think back to our itunes example (tylermcginnis.com/itunes).
-Notice that when you type in an artist name, iTunes gives us back a LOT of data.
-What they're really giving us is an Array full of Objects. It probably looks something
-like this. */
+Notice that when you type in an artist name, iTunes gives us back a 
+LOT of data. What they're really giving us is an Array 
+full of Objects. It probably looks something
+like this. 
+*/
 
 var data = [
     {
@@ -359,17 +384,20 @@ var data = [
 
 
 
-/*A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects)
+/*A very clean way to pass around large LISTS (arrays) of 
+COLLECTIONS (objects)
 of Data is to have an Array full of objects. */
 
 //Create an empty array called users.
 
-  //Code Here
+var users = [];
 
-/*Now add three user objects to your users array. Each user object should contain the
+/*Now add three user objects to your users array. 
+Each user object should contain the
 following properties. name, email, password, username.*/
 
 //include this as one of the objects in your array.
+
 var user1 = {
     name: 'Tyler McGinnis',
     email: 'tylermcginnis33@gmail.com',
@@ -377,17 +405,41 @@ var user1 = {
     username: 'infiniateLoop'
 };
 
-//Your Code Here
+var user2 = {
+  name: "alex",
+  email: "alexdecapri@gmail.com",
+  password: "djkfgdjkg",
+  username: "alexd"
+};
+
+var user3 = {
+  name: "jon",
+  email: "jonsemail@yes.com",
+  password: "asdfs",
+  username: "jonnnn"
+};
+
+users = [user1, user2, user3];
 
 /*Now you have a very common data structure. Twitter is a good use case.
-It's easy to imagine that your followers list on Twitter is an Array full or objects
-and those objects contain properties about the specific person you follow.*/
+It's easy to imagine that your followers list on Twitter 
+is an Array full or objects
+and those objects contain properties about the specific 
+person you follow.*/
 
-/*Now let's say that Tyler decided to delete his account. Loop through your array of
-objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
-Once you find the particular index he's located in, delete him from the array.*/
+/*Now let's say that Tyler decided to delete his account. 
+Loop through your array of
+objects until you find Tyler's account 
+(use tylermcginnis33@gmail.com to find him).
+Once you find the particular index he's located in, 
+delete him from the array.*/
 
-  //Code Here
+for (var i = 0; i < users.length; i++) {
+  debugger;
+  if (users[i].email === "tylermcginnis33@gmail.com") {
+    users.splice(users[i], 1);
+  }
+}
 
 //The activity we just did is very much how data works in 'the real world'.
 
